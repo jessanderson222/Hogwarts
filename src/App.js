25088239b0c63df202e347783c4fd12e1807bddb
jessanderson222 <jessanderson222@gmail.com>
2019-01-12 22:00:55 -0500

    character container line 9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 import "./App.css";
+import CharacterContainer from "./Containers/CharacterContainer";
+
+// import HouseContainer from "./Containers/HouseContainer";
 
 class App extends Component {
+  state = {
+    characterList: [],
+    searchCharacterTerm: ""
+  };
+
+  componentDidMount() {
+    const path = "http://localhost:3001/characters";
+    fetch(path)
+      .then(resp => resp.json())
+      .then(json => this.setState({ characterList: json }));
+  }
+
+  mutateState = () => {
+    let characterList = [...this.state.characterList];
+
+    return characterList;
+  };
+
   render() {
     return (
       <div className="app">
-        <h1>You Can Do This!</h1>
+        <h1>HI</h1>
+        <CharacterContainer characterList={this.state.characterList} />
       </div>
     );
   }
--- src/Containers/CharacterContainer.js
+++ src/Containers/CharacterContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import CharacterCard from "../Containers/CharacterContainer";
 
 export default class CharacterContainer extends React.Component {
   render() {
-    return <h1>Character Container</h1>;
+    const list = this.props.characterList.map(character => (
+      <CharacterCard key={character.id} character={character} />
+    ));
+    console.log("charactet container", list);
+    return (
+      <div>
+        <h3>Characters</h3>
+        {/* <ul>{list}</ul> */}
+      </div>
+    );
   }
 }
